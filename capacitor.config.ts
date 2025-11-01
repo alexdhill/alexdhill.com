0fe9569e8f59dab62e25a3f8808bc1e7d77d1c10
Alex D Hill <me@alexdhill.com>
2025-11-01 12:14:21 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'portfolio-final',
+  webDir: 'dist'
+};
+
+export default config;
